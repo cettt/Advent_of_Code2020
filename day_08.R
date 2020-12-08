a9bcd293e48d48be1a3bd4333dda741b1d6a464f
Cetin G <ceting@gmx.at>
2020-12-08 09:32:25 +0100

    Added day 08

---
+++ day_08.R
@@ -0,0 +1,30 @@
+data08 <- read.table("Input/day08.txt", sep = ";")[, 1]
+
+runcode <- function(mycode) {
+  check <- rep(0, length(mycode))
+  i <- 1
+  acc <- 0
+
+  while (TRUE) {
+    check[i] <- check[i] + 1
+    if (check[i] < 2) {
+      if (grepl("ac", mycode[i])) acc <- acc + readr::parse_number(mycode[i])
+      i <- i + if_else(grepl("j", mycode[i]), readr::parse_number(mycode[i]), 1)
+
+      if (i > length(mycode)) {
+        print(acc)
+        return(TRUE)
+      } # for part 2
+    } else return(acc)
+  }
+}
+
+# part1-----------
+runcode(data08)
+
+# part2----------
+for (j in which(!grepl("acc", data08))) {
+  x <- data08
+  x[j] <- paste(if_else(grepl("j", x[j]), "", "jmp"), readr::parse_number(x[j]))
+  y <- runcode(x)
+}
