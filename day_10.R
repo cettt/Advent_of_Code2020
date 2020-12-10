e070dd21216666ac4c34e35409fa23897e99e417
Cetin_G <ceting@gmx.at>
2020-12-10 09:55:55 +0100

    Added day 10

---
+++ day_10.R
@@ -0,0 +1,18 @@
+data10 <- read.table("Input/day10.txt")[,1]
+
+#part1-----------
+prod(table(diff(sort(c(0, data10, max(data10) + 3)))))
+
+#part2------------
+x <- sort(c(0, data10, max(data10) + 3))
+opt_x <- x[-c(1, which(diff(x) == 3), which(diff(x) == 3) + 1, length(x))]
+print(prod(pmin(2^table(cumsum(c(0, diff(opt_x) > 2))), 7)), 16)
+
+#x all adapters
+#opt_x all optional adapters: these do not have distance three to either of their 'neighbours'
+#setdiff(x, fix_x) all optional adapters
+#cumsum(c(0, diff(setdiff(x, fix_x)) > 2) assigns every optional adapter to a group
+#table(...)  these groups are of sizes 1, 2 and 3
+#2^table(...) number of combinations within each group
+#pmin(..., 7) in groups with size three it is not feasible to choose non of the optional adapters.
+#prod(....) take the product
