31d21e711fb2eea2ca6a8b978742114b07bd849c
Cetin_G <ceting@gmx.at>
2020-12-13 08:43:48 +0100

    Added day 13

---
+++ day_13.R
@@ -0,0 +1,18 @@
+data13 <- read.table("Input/day13.txt", sep = "")[,1]
+
+#part1---------
+bus_id <- as.numeric(stringr::str_extract_all(data13[2], "\\d+")[[1]])
+t_next_bus <- (floor(as.numeric(data13[1]) / bus_id) + 1) * bus_id
+
+bus_id[which.min(t_next_bus)] * (min(t_next_bus) - as.numeric(data13[1]))
+
+#part2--------------
+t_off <- which(strsplit(data13[2], ",")[[1]] != "x") - 1 #time offset
+res <- 1:2*bus_id[1]
+
+for (i in seq_along(bus_id)) {
+  bus_times <- seq(from = res[1], by = diff(res), length.out = 1e4)
+  res <- bus_times[which((bus_times + t_off[i]) %% bus_id[i] == 0)[1:2]]
+}
+
+print(res[1], 16)
