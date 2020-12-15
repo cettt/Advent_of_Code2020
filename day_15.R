1fe88de19a61dd102945cc731fdf050d1f9f1be5
Cetin_G <ceting@gmx.at>
2020-12-15 14:34:00 +0100

    Added day 15

--- day_15.R
+++ day_15.R
@@ -1,16 +1,16 @@
 data15 <- as.numeric(read.table("Input/day15.txt", sep = ",")[1, ])
 
 run_day15 <- function(n, input) {
-  tab_l <- rep(0, n) #tab_l[x] = t means that the last time x has been said was at time t
-  tab_l[input + 1] <- seq_along(input)
+  tab_t <- rep(0, n) #tab_t[x] = t means that the last time x-1 has been said was at time t
+  tab_t[input + 1] <- seq_along(input)
   y <- 0 #first number after the input sequence is zero
 
   for (turn in (length(input) + 1):n) {
-    y2 <- turn - tab_l[y + 1] #y2 candidate for the next number in the sequence (after y)
-    tab_l[y + 1] <- turn #update tab_l
+    y2 <- turn - tab_t[y + 1] #y2 candidate for the next number in the sequence (after y)
+    tab_t[y + 1] <- turn #update tab_t
     y <- y2 * (y2 != turn) #if y2 == turn then y had never been said before in which case y <- 0
   }
-  return(which(tab_l == n) - 1)
+  return(which(tab_t == n) - 1)
 }
 
 #part 1--------
