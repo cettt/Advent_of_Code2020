569dc71eb6b8ebce30abb55a9acf9f179cbdbaac
Cetin_G <ceting@gmx.at>
2020-12-21 22:10:33 +0100

    Cleaned code for day 20

--- day_20.R
+++ day_20.R
@@ -1,103 +1,97 @@
 library(tidyverse)
-data20 <- read_table("Input/day20.txt", col_names = "x")
 
-r100 <- as.numeric(t(matrix(1:100, nr = 10)) %*% diag(10)[,10:1])
-rotate_m <- function(m) matrix(m[r100], nr = 10)
-make_b <- function(m) {
-  list(b1 = m[1, ], b2 = m[, 10], b3 = m[10, ], b4 = m[, 1]) %>%
-    sapply(function(z) sum((z == "#") * 2^(9:0)))
+rotate_m <- function(m) {
+  k <- dim(m)[1]
+  matrix(m[t(matrix(1:k^2, nr = k)) %*% diag(k)[,k:1]], nrow = k)
 }
 
-x <- data20 %>%
-  mutate(gr = cumsum(grepl("Tile", x))) %>%
-  group_by(gr) %>%
-  mutate(id = parse_number(first(x))) %>%
+create_all_m <- function(m) {
+  lst(m1 = as.matrix(m), m2 = rotate_m(m1), m3 = rotate_m(m2), m4 = rotate_m(m3),
+      m5 = m1[, nrow(m1):1], m6 = m2[, nrow(m1):1], m7 = m3[, nrow(m1):1],
+      m8 = m4[, nrow(m1):1]) %>% tibble(m = .)
+}
+
+make_bound_id <- function(m) {
+  list(b_u = m[1, ], b_r = m[, ncol(m)], b_d = m[nrow(m), ], b_l = m[, 1]) %>%
+    lapply(function(z) sum((z == "#") * 2^((nrow(m)-1):0))) %>%
+    as_tibble()
+}
+
+data20 <- read_table("Input/day20.txt", col_names = "x", col_types = "c") %>%
+  mutate(tile = cumsum(grepl("Tile", x))) %>%
+  group_by(tile) %>%
+  mutate(tile = parse_number(first(x))) %>%
   slice(-1) %>%
-  separate(x, into = as.character(0:9), sep = 1:10) %>%
-  nest(im = `0`:`9`) %>%
+  {separate(., x, into = letters[seq_len(nchar(.$x[1]))], sep = seq_len(nchar(.$x[1])))} %>%
+  nest(m = -tile) %>%
   ungroup() %>%
+  mutate(m = map(m, create_all_m)) %>%
+  unnest(m) %>%
+  mutate(b = map(m, make_bound_id), id = 1:n()) %>%
+  unnest(b)
+
+tile_bound <- group_by(data20, tile) %>%
+  summarise(b = unique(c(b_u, b_r, b_d, b_l)), .groups = "drop")
+
+tile_neigbours <- distinct(data20, tile) %>%
   mutate(
-    m_1_0 = map(im, ~unname(as.matrix(.x))),
-    m_2_0 = map(m_1_0, rotate_m),
-    m_3_0 = map(m_2_0, rotate_m),
-    m_4_0 = map(m_3_0, rotate_m),
-    m_1_x = map(m_1_0, ~.x[, 10:1]),
-    m_2_x = map(m_2_0, ~.x[, 10:1]),
-    m_3_x = map(m_3_0, ~.x[, 10:1]),
-    m_4_x = map(m_4_0, ~.x[, 10:1]),
-    m_1_y = map(m_1_0, ~.x[10:1, ]),
-    m_2_y = map(m_2_0, ~.x[10:1, ]),
-    m_3_y = map(m_3_0, ~.x[10:1, ]),
-    m_4_y = map(m_4_0, ~.x[10:1, ]),
-    m_1_xy = map(m_1_x, ~.x[10:1, ]),
-    m_2_xy = map(m_2_x, ~.x[10:1, ]),
-    m_3_xy = map(m_3_x, ~.x[10:1, ]),
-    m_4_xy = map(m_4_x, ~.x[10:1, ])
-  ) %>%
-  select(-im, -gr) %>%
-  gather(hlp, m, -id) %>%
-  separate(hlp, into = c("dummy", "rot", "flip")) %>%
-  select(-dummy) %>%
-  mutate(b = map(m, make_b)) %>%
-  arrange(id, rot, flip) %>%
-  mutate(id2 = 1:n())
+    nei = map(tile, ~filter(tile_bound, tile == .x) %>% semi_join(tile_bound, ., by = "b") %>%
+                filter(tile != .x) %>% distinct(tile)),
+    n_nei = map_int(nei, nrow)
+  )
+#part1--------
+filter(tile_neigbours, n_nei == 2) %>% pull(tile) %>% prod() %>% print(16)
 
+#part2---------
+n <- sqrt(nrow(distinct(data20, tile)))
+immat2 <- matrix(0, nc = n, nr = n)
 
-show_matching <- function(.b, .id) {
-  x %>% filter(id != .id) %>%
-    select(id, b) %>%
-    filter(map_lgl(b, ~any(.x %in% .b))) %>%
-    distinct(id) %>%
-    pull(id)
+#backtracking function------
+find_tile <- function(i, j) {
+
+  if (!(i == 1 & j == 1)) {
+    if (i > 1) .bd <- filter(data20, id == immat2[i - 1, j]) %>% pull(b_d)
+    if (j > 1) .br <- filter(data20, id == immat2[i, j - 1]) %>% pull(b_r)
+
+    all_id <- data20 %>%
+      {`if`(i > 1, filter(., b_u == .bd), .)} %>%
+      {`if`(j > 1, filter(., b_l == .br), .)} %>%
+      anti_join(filter(data20, id %in% as.numeric(immat2)), by = "tile") %>%
+      filter(id > immat2[i, j]) %>% pull(id)
+
+  } else {
+    all_id <- tile_neigbours %>% filter(n_nei == 2) %>%
+      semi_join(data20, ., by = "tile") %>%
+      filter(id > immat2[i, j]) %>% pull(id)
+  }
+
+  if (length(all_id) == 0) {immat2[i, j] <<- 0; return(FALSE)}
+
+  immat2[i, j] <<- all_id[1]; return(TRUE)
 }
 
-y <- x %>%
-  mutate(
-    nei = map2(b, id, show_matching),
-    n_nei = map_int(nei, length)
-  )
 
-#part1--------
-y %>% filter(n_nei == 2) %>% distinct(id) %>% pull(id) %>% prod() %>% print(16)
-#part2---------
-#backtracking
-n <- sqrt(nrow(distinct(y, id)))
-immat2 <- matrix(0, nc = n, nr = n)
 k <- 1
-source("backtracking.R")
-
 while (k <= n^2) {
   i <- floor((k - 1) / n) + 1
-  j <- k - (i-1)*n
-  if (solve_cell(i, j)) {
-    k = k + 1
-  } else {
-    k = k - 1
-  }
+  j <- k - (i - 1) * n
+  k <- if (find_tile(i, j)) k + 1 else k - 1
 }
 
-tibble(id2 = as.numeric(immat2)) %>%
-  left_join(y %>% select(id, id2),  by ="id2") %>%
-  pull(id) %>%
-  matrix(nc = n)
-
-immat <- matrix(".", nrow = (10*n), ncol = (10*n))
+n2 <- nrow(data20$m[[1]]) - 2
+immat <- matrix(".", nc = n2 * n, nr = n2 * n)
 for (i in seq_len(n)) {
   for (j in seq_len(n)) {
-    immat[((i-1)*10 + 1):(i*10), ((j-1)*10 + 1):(j*10)] <- y %>%
-      filter(id2 == immat2[i, j]) %>% select(m) %>% unlist() %>% matrix(nc = 10)
+    immat[((i-1)*n2 + 1):(i*n2), ((j-1)*n2 + 1):(j*n2)] <-
+      filter(data20, id == immat2[i, j])$m[[1]][1:n2 + 1, 1:n2 + 1]
   }
 }
-
-immat_nob <- immat[seq_len(10*n) %% 10 > 1, seq_len(10*n) %% 10 > 1] #image without borders
 
 mon <- read.table("Input/day20_monster.txt", comment.char = "", sep = ";")[,1] %>%
   {tibble(mon = str_pad(., 20, "right", " "))} %>%
   separate(mon, into = letters[1:20], sep = 1:20) %>%
   as.matrix() %>%
   {which(. == "#")}
-
-im <- immat_nob
 
 check_im <- function(im) {
   counter <- 0
@@ -110,35 +104,8 @@ check_im <- function(im) {
   return(counter)
 }
 
-k <- dim(immat_nob)[1]
-rotate_m2 <- function(m) {
-  rmat <- as.numeric(t(matrix(1:k^2, nr = k)) %*% diag(k)[,k:1])
-  matrix(m[rmat], nr = k)
-}
-
-tibble(m_1_0 = list(immat_nob)) %>%
-  mutate(
-    m_2_0 = map(m_1_0, rotate_m2),
-    m_3_0 = map(m_2_0, rotate_m2),
-    m_4_0 = map(m_3_0, rotate_m2),
-    m_1_x = map(m_1_0, ~.x[, k:1]),
-    m_2_x = map(m_2_0, ~.x[, k:1]),
-    m_3_x = map(m_3_0, ~.x[, k:1]),
-    m_4_x = map(m_4_0, ~.x[, k:1]),
-    m_1_y = map(m_1_0, ~.x[k:1, ]),
-    m_2_y = map(m_2_0, ~.x[k:1, ]),
-    m_3_y = map(m_3_0, ~.x[k:1, ]),
-    m_4_y = map(m_4_0, ~.x[k:1, ]),
-    m_1_xy = map(m_1_x, ~.x[k:1, ]),
-    m_2_xy = map(m_2_x, ~.x[k:1, ]),
-    m_3_xy = map(m_3_x, ~.x[k:1, ]),
-    m_4_xy = map(m_4_x, ~.x[k:1, ])
-  ) %>%
-  gather(hlp, m) %>%
-  separate(hlp, into = c("dummy", "rot", "flip")) %>%
-  select(-dummy) %>%
+create_all_m(immat) %>%
   mutate(n_monst = map_dbl(m, check_im)) %>%
   filter(n_monst > 0) %>%
-  slice(1) %>%
-  mutate(res = map2_dbl(m, n_monst, ~sum(.x == "#") - .y*length(mon))) %>%
+  mutate(res = map2_dbl(m, n_monst, ~sum(.x == "#") - .y * length(mon))) %>%
   pull(res)
