59572d67c80458a1f70682391380b84a0c3691f6
Cetin_G <ceting@gmx.at>
2021-01-02 09:53:26 +0100

    Rewritten day07 in base R

--- day_07.R
+++ day_07.R
@@ -1,18 +1,16 @@
 library(tidyverse)
 
-data07 <- read_table("Input/day07.txt", col_names = FALSE, col_types = "c") %>%
-  separate(1, into = c("outer", "x"), sep = " contain ") %>%
-  separate(x, into = letters[1:4], sep = ", ", fill = "right") %>%
-  gather(dummy, inner, -outer, na.rm = TRUE) %>%
-  mutate(
-    n = parse_number(sub("no other bag", "0", inner)),
-    across(c(inner, outer), ~sub("\\d ", "", sub("bags?\\.?", "bag", .))), #remove digits and plural s
-  )
+data07 <- strsplit(gsub("bags?\\.?", "bag", readLines("Input/day07.txt")), " contain ")
+
+bags_df <- do.call(rbind, lapply(data07, function(z) {
+  y <- strsplit(z[2], ", ")[[1]]
+  n <- as.integer(gsub("\\D+", "", y))
+  data.frame(outer = z[1], inner = gsub("\\d ", "", y), n = n)
+}))
 
 # part1-----------
 find_outer_bags <- function(inner_bag) {
-
-  outer_bags <- data07 %>% filter(inner %in% inner_bag) %>% pull(outer)
+  outer_bags <- subset(bags_df, inner %in% inner_bag)[, 1]
   if (length(outer_bags) == 0) return(NULL)
   return(unique(c(outer_bags, find_outer_bags(outer_bags))))
 }
@@ -22,9 +20,10 @@ length(find_outer_bags("shiny gold bag"))
 #part2----------
 count_inner_bags <- function(outer_bag) {
 
-  data07 %>% filter(outer == outer_bag) %>%
-    summarise(x = sum(n + n * map_dbl(inner, count_inner_bags))) %>% # sum of empty col returns zero
-    pull(x)
+  x <- subset(bags_df, outer == outer_bag)
+  if (nrow(x) == 0) return(0)
+
+  sum(sapply(seq_len(nrow(x)), function(i) x[i, 3] * (1 + count_inner_bags(x[i, 2]))), na.rm = TRUE)
 }
 
-count_inner_bags("shiny gold bag")
+count_inner_bags("shiny gold bag") #30899
